97ee56e014e7584f139c73f24e171f676bfdc199
Yubaraj Magar <yubaraj.100mgr@gmail.com>
2022-10-06 15:53:27 +1100

    all the packages and cdn are done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 import "./App.css";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      Hello World{" "}
+      <Button variant="danger">
+        Some Button <i class="fa-solid fa-clock"></i>
+      </Button>
+    </div>
+  );
 };
 
 export default App;
